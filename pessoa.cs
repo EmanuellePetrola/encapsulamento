274b765f005d13805fbca879e369324a953754cf
EmanuellePetrola <emanuelle.petrola@edu.senai.br>
2026-09-10 15:30:55 -0300

    Adicionar arquivos de projeto.

---
+++ pessoa.cs
@@ -0,0 +1,19 @@
+﻿using Encapsulamento;
+
+Pessoa.cs *
+namespace Encapsulamento
+{
+    internal class Pessoa
+    {
+        private string _nome;
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                _nome = value;
+            }
+        }
+    }
+}
