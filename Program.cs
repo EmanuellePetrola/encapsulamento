070f5fb3fa3fbc5834fba6b8003fa9d4e3a20c1e
EmanuellePetrola <emanuelle.petrola@edu.senai.br>
2026-09-11 09:35:25 -0300

    atividade

--- Program.cs
+++ Program.cs
@@ -1,20 +1,10 @@
-﻿internal class Pessoa
-{
-    private string _nome;
-    public string Nome
-    {
-        get { return _nome; }
-        set
-        {
-            _nome = value;
-        }
-    }
-}
-}
-
-Pessoa p = new Pessoa();
-p.Nome = "Marcos";
-Console.WriteLine(p.Nome);
+﻿Termometro meuTermometro = new Termometro();
 
 
+meuTermometro.Celsius = 25;
 
+Console.WriteLine("Celsius: " + meuTermometro.Celsius);
+
+Console.WriteLine("Fahrenheit: " + meuTermometro.Fahrenheit);
+
+Console.WriteLine("Kelvin: " + meuTermometro.Kelvin);
